5815a7b3c595f963ea35c56c93a04f186ee91b6b
LTaylor137 <61397207+LTaylor137@users.noreply.github.com>
2020-08-06 16:05:10 +1000

    basic functions working. add unique images later.

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AddressCardComponent implements OnInit {
 
   @Input()
+  image: string = "Image";
+  @Input()
   title: string = "Title";
   @Input()
   name: string = "Name";
